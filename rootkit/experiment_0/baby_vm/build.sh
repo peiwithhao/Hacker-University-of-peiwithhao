fd52a9403575dcfc8e3260eaed48c527fe026955
peiandhao <2689271477@qq.com>
2025-01-06 15:19:02 +0800

    规范化第二部分

--- rootkit/experiment_0/baby_vm/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ..
-make
-cd baby_vm
-cp ../pwhrootkit.ko ./fs_extract/
-cd fs_extract
-find . | cpio -o --format=newc >../rootfs.cpio
--- rootkit/experiment_0/baby_vm/debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gdb -ex "directory /home/peiwithhao/Kernel/kernel_source/linux-6.3.4" \
-    -ex "file vmlinux" \
-    -ex "target remote localhost:1234"
