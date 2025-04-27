a7d3db4cb9f088896ccde2f79ff8ea0cb39725f7
peiandhao <2689271477@qq.com>
2025-04-27 11:00:46 +0800

    格式规整

--- rootkit/baby_vm/debug.sh
+++ rootkit/baby_vm/debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gdb -ex "directory /home/peiwithhao/Kernel/kernel_source/linux-6.3.4" \
     -ex "add-symbol-file ./fs_extract/pwhkit.ko 0xffffffffc0000000" \
-    -ex "file vmlinux" \
+    -ex "file ~/vmlinux" \
     -ex "target remote localhost:1234"
