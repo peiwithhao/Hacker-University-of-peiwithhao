cefb80192abdd8de5e7a022386540baac0bc9b13
peiandhao <2689271477@qq.com>
2025-09-26 10:31:41 +0800

    1. 流量检测以及限制程序组件完成 后续工作： 1. 进行故障注入的逻辑实现

--- trace/eBPF/APPS/traffic_monitor/traffic_monitor.bpf.h
+++ trace/eBPF/APPS/traffic_monitor/traffic_monitor.bpf.h
@@ -17,26 +17,41 @@
 
 // Credit
 #define INIT_CREDIT (INITIAL_CREDIT_PACKETS * MAX_BYTES_PER_PACKET)
+/* #define INIT_CREDIT 10 */
 #define MAX_CREDIT (100 * MAX_BYTES_PER_PACKET)
 
 #define CREDIT_PER_NS(delta, rate) ((((u64)(delta)) * (rate)) >> 20)
 
-// Thresh: for control the traffic
+// Thresh: for controll the traffic
 #define MARK_THRESH (40 * MAX_BYTES_PER_PACKET)
 #define DROP_THRESH (80 * 5 * MAX_BYTES_PER_PACKET)
 #define LARGE_PKT_DROP_THRESH (DROP_THRESH - (15 * MAX_BYTES_PER_PACKET))
+#define MARK_REGION_SIZE (LARGE_PKT_DROP_THRESH - MARK_THRESH)
+#define LARGE_PKT_THRESH 120
 
-
+#define BURST_SIZE_NS   100000  // 100us
+#define MARK_THRESH_NS  50000   // 50us
+#define DROP_THRESH_NS  500000   // 500us
+                               
 
 
 /* 用来指示当前cgroup最近访问时间以及速率 */
+/* struct { */
+/* 	__uint(type, BPF_MAP_TYPE_CGRP_STORAGE); */
+/*     __uint(map_flags, BPF_F_NO_PREALLOC); */
+/* 	__uint(max_entries, 0); */
+/* 	__type(key, int); */
+/*     /1* __type(value, struct traffic_vqueue); *1/ */
+/*     __type(value, long); */
+/* } queue_state SEC(".maps"); */
+
 struct {
-	__uint(type, BPF_MAP_TYPE_CGRP_STORAGE);
-    __uint(map_flags, BPF_F_NO_PREALLOC);
-	__uint(max_entries, 0);
-	__type(key, struct bpf_cgroup_storage_key);
-    __type(value, struct traffic_vqueue);
-} queue_state SEC(".maps");
+        __uint(type, BPF_MAP_TYPE_CGRP_STORAGE);
+        __uint(map_flags, BPF_F_NO_PREALLOC);
+        __type(key, int);
+        __type(value, struct traffic_vqueue);
+} cgrp_storage SEC(".maps");
+
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
@@ -47,12 +62,12 @@ struct {
 
 
 struct traffic_pkt_info {
-    int cwnd;
+    int cwnd; //拥塞窗口大小
     int rtt;
     int packets_out;
     bool is_ip;
     bool is_tcp;
-    short ecn; //显示拥塞
+    short ecn; //标记是否支持ecn
 };
 
 static int get_tcp_info(struct __sk_buff *skb, struct traffic_pkt_info *pkti){
@@ -109,11 +124,74 @@ static void traffic_get_pkt_info(struct __sk_buff *skb, struct traffic_pkt_info 
     if (pkti->is_tcp)
         get_tcp_info(skb, pkti);
 }
-static void traffic_vqueue_init(struct traffic_vqueue *tvq, int rate){
+static __always_inline void traffic_vqueue_init(struct traffic_vqueue *tvq, int rate){
     bpf_printk("Initializing queue_state, rate:%d\n", rate * 128);
     tvq->lasttime = bpf_ktime_get_ns();
-    tvq->credit = INIT_CREDIT;
+    tvq->credit = (__s32)INIT_CREDIT;
     tvq->rate = rate * 128;
+}
+
+static __always_inline void traffic_vqueue_edit(struct traffic_vqueue *tvq, int rate) {
+    unsigned long long curtime;
+    curtime = bpf_ktime_get_ns();
+    tvq->lasttime = curtime - BURST_SIZE_NS;
+    tvq->credit = 0;
+    tvq->rate = rate * 128;
+} 
+
+static __always_inline void traffic_update_stats(struct traffic_queue_stats *tqs, 
+                                                 int len,
+                                                 unsigned long long curtime,
+                                                 bool congestion_flag, 
+                                                 bool drop_flag,
+                                                 bool cwr_flag,
+                                                 bool ecn_ce_flag,
+                                                 struct traffic_pkt_info *pkti,
+                                                 int credit)
+{
+    int rv = ALLOW_PKT;
+    if (tqs != NULL) {
+        // barrier
+        __sync_add_and_fetch(&(tqs->bytes_total), len);
+        if (tqs->stats){
+            if (tqs->firstPacketTime == 0)
+                tqs->firstPacketTime = curtime;
+            tqs->lastPacketTime = curtime;
+            __sync_add_and_fetch(&(tqs->pkts_total), 1);
+            if (congestion_flag) {
+                __sync_add_and_fetch(&(tqs->pkts_marked), 1);
+                __sync_add_and_fetch(&(tqs->bytes_marked), len);
+            }
+            if (drop_flag) {
+                __sync_add_and_fetch(&(tqs->pkts_dropped), 1);
+                __sync_add_and_fetch(&(tqs->bytes_dropped), len);
+            }
+            if (ecn_ce_flag) {
+                __sync_add_and_fetch(&(tqs->pkts_ecn_ce), 1);
+            }
+            if (pkti->cwnd) {
+                __sync_add_and_fetch(&(tqs->sum_cwnd), pkti->cwnd);
+                __sync_add_and_fetch(&(tqs->sum_cwnd_cnt), 1);
+            }
+            if (pkti->rtt) {
+                __sync_add_and_fetch(&(tqs->sum_rtt), pkti->rtt);
+            }
+            __sync_add_and_fetch(&(tqs->sum_credit), credit);
+            if (drop_flag)
+                rv = DROP_PKT;
+            if (cwr_flag)
+                rv |= 2;
+            if (rv == DROP_PKT) {
+                __sync_add_and_fetch(&(tqs->returnValCount[0]), 1);
+            }else if (rv == ALLOW_PKT) {
+                __sync_add_and_fetch(&(tqs->returnValCount[1]), 1);
+            }else if (rv == 2) {
+                __sync_add_and_fetch(&(tqs->returnValCount[2]), 1);
+            }else if (rv == 3) {
+                __sync_add_and_fetch(&(tqs->returnValCount[3]), 1);
+            }
+        }
+    }
 }
 
 #endif
--- trace/eBPF/APPS/traffic_monitor/traffic_monitor.h
+++ trace/eBPF/APPS/traffic_monitor/traffic_monitor.h
@@ -2,14 +2,30 @@
 #define __TRAFFIC_MONITOR_H
 
 struct traffic_vqueue{
-    unsigned long long lasttime;
+    struct bpf_spin_lock lock;
+    __u64 lasttime;
     int credit;     /* bytes */
-    unsigned int rate;  /* In byte per NS << 20 */
+    __u32 rate;  /* In byte per NS << 20 */
 };
 
 struct traffic_queue_stats {
     unsigned long rate;
-    unsigned long stats:1, loopback:1;
+    unsigned long stats:1, loopback:1, no_cn:1;
+    unsigned long long bytes_total;
+    unsigned long long pkts_total;
+    unsigned long long pkts_marked;
+    unsigned long long bytes_marked;
+    unsigned long long pkts_ecn_ce;
+    unsigned long long pkts_dropped;
+    unsigned long long bytes_dropped;
+    unsigned long long sum_cwnd;
+    unsigned long long sum_cwnd_cnt;
+    unsigned long long sum_rtt;
+
+    unsigned long long firstPacketTime;
+    unsigned long long lastPacketTime;
+
+    unsigned long long returnValCount[4];
     long long sum_credit;
 };
 #endif
